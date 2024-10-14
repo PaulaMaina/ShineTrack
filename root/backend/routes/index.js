51ef49ca32df6cca33158fc38522f0db0175b09d
PaulaMaina <kaburamainap@gmail.com>
2024-10-14 22:49:12 +0300

    Uncommented the business registration route

--- root/backend/routes/index.js
+++ root/backend/routes/index.js
@@ -1,16 +1,15 @@
 const BusinessController = require('../controllers/BusinessController');
+const express = require('express');
 const UserController = require('../controllers/UserController');
 
 const routes = (app) => {
-    app.get('/', (_req, res) => {
-        res.send('Hello from ShineTrack API server');
-    });
+    app = express.Router();
 
     app.post('/register', UserController.registerUser);
     app.post('/login', UserController.loginUser);
 
     //app.get('/businesses', BusinessController);
-    //app.post('new-business', BusinessController.registerBusiness);
+    app.post('/new-business', BusinessController.registerBusiness);
 }
 
 module.exports = routes;
