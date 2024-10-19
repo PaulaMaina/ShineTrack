6f82eba5f1e585cf56f159ce5c126922ec1b2af9
PaulaMaina <kaburamainap@gmail.com>
2024-10-19 14:56:42 +0300

    Created an employees page, controller and functions related to employees

--- root/controllers/EmployeeController.js
+++ root/controllers/EmployeeController.js
@@ -8,6 +8,7 @@ class EmployeeController {
             const lastName = req.body.lastName;
             const phoneNumber = req.body.phoneNumber;
             const salary = req.body.salary;
+            const business = req.body.business;
             const employeeExists = await Employee.findOne({ idNumber });
             
             if (employeeExists){
@@ -23,7 +24,8 @@ class EmployeeController {
                 lastName: lastName,
                 phoneNumber: phoneNumber,
                 salary: salary,
-                business: req.business._id,
+                business: business,
+                user: req.user._id
             };
 
             const employee = await Employee.create(newEmployee);
@@ -38,13 +40,56 @@ class EmployeeController {
     static async displayEmployees(req, res) {
         try {
             // Retrieves all the employee registered under a specific business
-            const employees = await Employee.find({ user: req.business._id });
+            const employees = await Employee.find({ user: req.user._id });
 
             // Send employees to the frontend
             res.status(200).json(employees);
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'An error occurred while fetching the employees'});
+        }
+    };
+
+    static async updateEmployee(req, res) {
+        try {
+            const employee = await Employee.findById(req.params.id);
+
+            if (employee) {
+                if (employee.user.toString() === req.user._id.toString()) {
+                    const employeeUpdate = await Employee.findByIdAndUpdate(req.params.id, req.body, {
+                        new: true,
+                        runValidators: true
+                    });
+
+                    return res.status(200).json(employeeUpdate);
+                } else {
+                    return res.status(401).json({ message: 'This user is not authorized to update employee details' });
+                }
+            } else {
+                return res.status(404).json({ message: 'Employee not found'});
+            }
+        } catch (error) {
+            return res.json(500).json({ message: 'A server error occurred while updating the employee details' });
+        }
+    };
+
+    static async deleteEmployee(req, res) {
+        try {
+            const employee = await Employee.findById(req.params.id);
+
+            if (employee) {
+                if (employee.user.toString() === req.user._id.toString()) {
+                    await employee.deleteOne();
+                    return res.status(200).json({ message: 'Employee deleted successfully' });
+                } else {
+                    return res.status(401).json({ message: 'This user is not authorized to delete employee details' });
+                }
+            } else {
+                return res.status(404).json({ message: 'Employee not found'});
+            }
+        } catch (error) {
+            console.error('Error deleting employee:', error);
+            return res.status(500).json({ message: 'A server error occurred while deleting the employee' });
         }
     };
 }
---
+++ root/public/js/employeeFunc.js
@@ -0,0 +1,168 @@
+// Retrieves the login token from localStorage
+const token = localStorage.getItem('token');
+
+async function showEmployees() {
+    if (token) {
+        try {
+            const res = await fetch('http://localhost:3000/api/employees/myemployees', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            const data = await res.json();
+
+            if (Array.isArray(data)) {
+                const tableBody = document.querySelector('#myemployees tbody');
+                let counter = 1;
+
+                tableBody.innerHTML = '';
+
+                data.forEach(employee => {
+                    const row = document.createElement('tr');
+
+                    row.innerHTML = `
+                        <td>${counter}</td>
+                        <td>${employee.idNumber}</td>
+                        <td>${employee.firstName} ${employee.lastName}</td>
+                        <td>${employee.phoneNumber}</td>
+                        <td>${employee.salary}</td>
+                        <td>
+                            <button id="edit" onclick="editEmployee('${employee._id}')">Edit</button>
+                            <button id="delete" onclick="deleteEmployee('${employee._id}')">Delete</button>
+                        </td>
+                    `;
+
+                    tableBody.appendChild(row);
+                    counter += 1;
+                });
+            } else {
+                document.getElementById('outputMessage').innerText = 'No employees found. Register one today!';
+            }
+        } catch (error) {
+            alert('An error occurred while fetching your employees. Please try again');
+            console.error('Error:', error);
+        }
+    } else {
+        alert('The user is not authenticated. Please log in.');
+    }
+}
+
+//Makes an authenticated request to register a new employee
+if (token) {
+    document.getElementById('employee-register').addEventListener('submit', async function (e) {
+        e.preventDefault();
+
+        const idNumber = document.getElementById('idNumber').value;
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const phoneNumber = document.getElementById('phoneNumber').value;
+        const salary = document.getElementById('salary').value;
+        const business = document.getElementById('bizList-dropdown').value;
+
+        const formData = {
+            idNumber,
+            firstName,
+            lastName,
+            phoneNumber,
+            salary,
+            business
+        };
+
+        try {
+            const res = await fetch('http://localhost:3000/api/employees/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const data = await res.json();
+
+            //Checks whether the employee registration was successful
+            if (res.ok) {
+                document.getElementById('outputMessage').innerText = 'Registration has been completed successfully!';
+                
+                //Redirects to the employees page after 2 seconds
+                setTimeout(() => {
+                    window.location.href = '/employees';
+                }, 2000);
+            } else {
+                document.getElementById('outputMessage').innerText = data.message;     //Displays the message from the backend
+            }
+        } catch (error) {
+            document.getElementById('outputMessage').innerText = 'An error occurred during registration. Please try again';
+            console.error('Error:', error);
+        }
+    });
+}
+
+// Makes an authenticated request to edit details of a specific employee
+async function editEmployee(id) {
+    const newFirstName = prompt('Enter a new first name: ');
+    const newLastName = prompt('Enter a new last name: ');
+    const newPhoneNumber = prompt('Enter a new phone number: ');
+
+    if (newFirstName && newLastName && newPhoneNumber) {
+        const bizUpdate = {
+            firstName: newFirstName,
+            lastName: newLastName,
+            phoneNumber: newPhoneNumber
+        };
+
+        try {
+            const res = await fetch(`/api/employees/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(bizUpdate)
+            });
+
+            if (res.ok) {
+                alert('The employee details have been updated successfully');
+                showEmployees();
+            } else {
+                const data = await res.json();
+
+                alert('An error occurred: ' + data.message);
+            }
+        } catch (error) {
+            console.error('An error occurred: ', error);
+        }
+    }
+}
+
+// Makes an authenticated request to delete an employees
+async function deleteEmployee(id) {
+    if (confirm('Are you sure you want to delete this employee?')) {
+        try {
+            const res = await fetch(`/api/employees/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (res.ok) {
+                alert('The employee selected has been deleted successfully');
+                showEmployees();
+            } else {
+                const data = await res.json();
+
+                alert('Error: ' + data.message);
+            }
+        } catch (error) {
+            console.error('An error occurred: ', error);
+        }
+    };
+}
+
+// Loads the user's employees
+window.onload = showEmployees;
